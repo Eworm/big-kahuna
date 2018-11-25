955bfb0dd1c11e171505406e41c5deb322e9476d
Wout <wout@woutmager.nl>
2018-11-25 22:00:30 +0100

    Adding the branches template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,10 +24,6 @@
             @endforeach
         ]
 
-        // Statamic.Publish = {
-        //     pages: {!! json_encode($pages) !!}
-        // };
-
     </script>
 
     <div class="flexy mb-24">
@@ -75,51 +71,6 @@
                 </div>
             </div>
 
-            {{-- <div class="card flush">
-                <div class="dossier-table-wrapper">
-                    <table class="dossier has-checkboxes">
-                        <thead>
-                            <tr>
-                                <th colspan="2">
-                                    Content
-                                </th>
-
-                            </tr>
-                            <tr>
-                                <th colspan="2">
-                                    <input v-model="query" type="search" placeholder="Search for a page, entry or collection" class="form-control">
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody id="tbody">
-
-                            <tr v-for="(index, page) of pages">
-                                <td class="checkbox-col">
-                                    <input type="checkbox" id="checkbox-@{{index}}">
-                                    <label for="checkbox-@{{index}}"></label>
-                                </td>
-                                <td class="cell-title first-cell">
-                                    <span class="column-label"></span>
-                                    @{{ page.title }} <span class="text-muted">(@{{page.type}})</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="2">
-                                    <button  type="button" class="btn float-left">
-                                        Select all
-                                    </button>
-                                    <button type="button" class="btn btn-primary add-row float-right">
-                                    Add to menu
-                                    <i class="icon icon-plus icon-right"></i>
-                                </button>
-                                </td>
-                        </tfoot>
-                    </table>
-                </div>
-            </div> --}}
-
             <div class="card flush">
                 <div class="head">
                     Aangepaste links
@@ -131,6 +82,112 @@
         <div class="col-md-7">
 
             <div class="card flush">
+
+                <div class="page-tree">
+
+                    {{-- <script type="application/json">
+                    {
+                      "nestables": [
+                        {
+                          "id": "72c016c6-cc0a-4928-b53b-3275f3f6da0a",
+                          "order": 1,
+                          "title": "About Me",
+                          "url": "/about",
+                          "uri": "/about",
+                          "extension": "md",
+                          "edit_url": "http://statamic.localhost/cp/pages/edit/about",
+                          "create_child_url": "http://statamic.localhost/cp/pages/create/about",
+                          "slug": "about",
+                          "published": true,
+                          "has_entries": false,
+                          "create_entry_url": "http://statamic.localhost/cp/collections/entries//create",
+                          "entries_url": "http://statamic.localhost/cp/collections/entries",
+                          "collapsed": false,
+                          "items": []
+                        },
+                        {
+                          "id": "60962021-f154-4cd2-a1d7-035a12b6da9e",
+                          "order": 2,
+                          "title": "My Magical Blogventure",
+                          "url": "/blog",
+                          "uri": "/blog",
+                          "extension": "md",
+                          "edit_url": "http://statamic.localhost/cp/pages/edit/blog",
+                          "create_child_url": "http://statamic.localhost/cp/pages/create/blog",
+                          "slug": "blog",
+                          "published": true,
+                          "has_entries": true,
+                          "create_entry_url": "http://statamic.localhost/cp/collections/entries/blog/create",
+                          "entries_url": "http://statamic.localhost/cp/collections/entries/blog",
+                          "collapsed": false,
+                          "items": []
+                        },
+                        {
+                          "id": "3cd2d431-699c-417c-8d57-9183cd17a6fc",
+                          "order": 3,
+                          "title": "Gallery",
+                          "url": "/gallery",
+                          "uri": "/gallery",
+                          "extension": "md",
+                          "edit_url": "http://statamic.localhost/cp/pages/edit/gallery",
+                          "create_child_url": "http://statamic.localhost/cp/pages/create/gallery",
+                          "slug": "gallery",
+                          "published": true,
+                          "has_entries": false,
+                          "create_entry_url": "http://statamic.localhost/cp/collections/entries//create",
+                          "entries_url": "http://statamic.localhost/cp/collections/entries",
+                          "collapsed": false,
+                          "items": []
+                        },
+                        {
+                          "id": "26a4ce21-d768-440d-806b-213918df0ee0",
+                          "order": 4,
+                          "title": "Favorite Things",
+                          "url": "/things",
+                          "uri": "/things",
+                          "extension": "md",
+                          "edit_url": "http://statamic.localhost/cp/pages/edit/things",
+                          "create_child_url": "http://statamic.localhost/cp/pages/create/things",
+                          "slug": "things",
+                          "published": true,
+                          "has_entries": true,
+                          "create_entry_url": "http://statamic.localhost/cp/collections/entries/things/create",
+                          "entries_url": "http://statamic.localhost/cp/collections/entries/things",
+                          "collapsed": false,
+                          "items": []
+                        },
+                        {
+                          "id": "de627bca-7595-429e-9b41-ad58703916d7",
+                          "order": 5,
+                          "title": "Contact",
+                          "url": "/contact",
+                          "uri": "/contact",
+                          "extension": "md",
+                          "edit_url": "http://statamic.localhost/cp/pages/edit/contact",
+                          "create_child_url": "http://statamic.localhost/cp/pages/create/contact",
+                          "slug": "contact",
+                          "published": true,
+                          "has_entries": false,
+                          "create_entry_url": "http://statamic.localhost/cp/collections/entries//create",
+                          "entries_url": "http://statamic.localhost/cp/collections/entries",
+                          "collapsed": false,
+                          "items": []
+                        }
+                      ]
+                    }
+                    </script> --}}
+
+                    <page-tree inline-template v-cloak>
+
+                        <div :class="{'page-tree': true, 'show-urls': showUrls}">
+
+                            <branches :pages="pages" :depth="1" :sortable="isSortable" :dirty="changed"></branches>
+
+                        </div>
+
+                    </page-tree>
+
+                </div>
 
             </div>
 
