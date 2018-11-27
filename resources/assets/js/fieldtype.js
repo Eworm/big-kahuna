9a955924b0f84be556b9be82c0c8c9e9b7c676ce
Wout <wout@woutmager.nl>
2018-11-27 21:36:29 +0100

    Made the list of entries a component

--- resources/assets/js/fieldtype.js
+++ resources/assets/js/fieldtype.js
@@ -2,16 +2,41 @@ Vue.component('menus-fieldtype', {
 
     mixins: [Fieldtype],
 
-    template: `<tr v-for="(index, page) of allpages">
-                <td class="checkbox-col">
-                <input type="checkbox" id="checkbox-{{index}}">
-                <label for="checkbox-{{index}}"></label>
-                </td>
-                <td class="cell-title first-cell">
-                <span class="column-label"></span>
-                {{ page.title }} <span class="text-muted">({{page.type}})</span>
-                </td>
-                </tr>`,
+    template: `<table class="dossier has-checkboxes">
+                <thead>
+                    <tr>
+                        <th colspan="2">
+                            Content
+                        </th>
+                    </tr>
+                    <tr>
+                        <th colspan="2">
+                            <input v-model="query" type="search" placeholder="Search for a page, entry or collection" class="form-control">
+                        </th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                <tr v-for="(index, page) of allpages">
+                        <td class="checkbox-col">
+                        <input type="checkbox" id="checkbox-{{index}}">
+                        <label for="checkbox-{{index}}"></label>
+                        </td>
+                        <td class="cell-title first-cell">
+                        <span class="column-label"></span>
+                        {{ page.title }} <span class="text-muted">({{page.type}})</span>
+                        </td>
+                        </tr></tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="2">
+                                    <button type="button" class="btn btn-primary add-row float-right">
+                                        Add to menu
+                                        <i class="icon icon-plus icon-right"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>`,
 
     data: function() {
         return {
