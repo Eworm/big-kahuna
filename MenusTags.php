063d698bc73f2bb50f645babf8670c6261dfbd61
Wout <wout@woutmager.nl>
2018-12-02 21:28:52 +0100

    Adding parameters to the menu tag

--- MenusTags.php
+++ MenusTags.php
@@ -14,16 +14,33 @@ class MenusTags extends Tags
     public function index()
     {
         //
-        $pages = $this->storage->getJSON('pages');
+        $pages          = $this->storage->getJSON('pages');
+        $id             = $this->getParam('id');
+        $class          = $this->getParam('class');
+        $activeClass    = $this->getParam('active_class');
+        $itemClass      = $this->getParam('item_class');
+        $submenuClass      = $this->getParam('submenu_class');
 
         //
         $html = '';
-        $html .= '<ul class="nav navbar-nav">';
+        $html .= '<ul id="' . $id . '" class="' . $class . '">';
         foreach ($pages as $page) {
-            $html .= '<li>';
+            $html .= '<li class="' . $itemClass . '">';
             $html .= '<a href="' . $page["url"] . '" title="">';
             $html .= $page["title"];
             $html .= '</a>';
+
+            $html .= '<ul class="' . $submenuClass . '">';
+            foreach ($page['items'] as $item) {
+
+                $html .= '<li class="">';
+                $html .= '<a href="' . $item["url"] . '" title="">';
+                $html .= $item["title"];
+                $html .= '</a>';
+                $html .= '</li>';
+            }
+            $html .= '</ul>';
+
             $html .= '</li>';
         }
         $html .= '</ul>';
