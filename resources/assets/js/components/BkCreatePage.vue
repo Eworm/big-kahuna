14265eb9c41c5602c08f0881b3919c057d9ee3dd
Wout <wout@woutmager.nl>
2018-12-07 10:19:43 +0100

    Split everything into separate vue files

---
+++ resources/assets/js/components/BkCreatePage.vue
@@ -0,0 +1,26 @@
+props: [],
+
+data: function() {
+    return {
+        menuName: ''
+    }
+},
+
+methods: {
+    saveMenu: function() {
+        if (this.menuName !== '') {
+            this.$http.post(
+                cp_url("addons/big-kahuna/store"), {
+                    menu_name: this.menuName
+                },
+                function(res) {
+                    location.href = cp_url('addons/big-kahuna/edit/') + res.menu
+                }
+            )
+        } else {
+            this.$dispatch("setFlashError", 'Enter a menu name')
+        }
+    }
+},
+
+ready: function() {}
