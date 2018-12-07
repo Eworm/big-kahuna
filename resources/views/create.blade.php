efc76c8f56375819042cee7c5de38d2eb526d24a
Wout <wout@woutmager.nl>
2018-12-07 09:37:09 +0100

    Some tweaks & changes

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,24 +2,27 @@
 
 @section('content')
 
-<menus-create-page inline-template>
-    <div>
-        <div class="sticky flexy mb-3 full-width">
-            <h1 class="fill"> Add Menu </h1>
-            <button type="button" class="btn btn-primary" v-on:click="saveMenu">Create Menu</button>
-        </div>
-        <div class="px-3">
-            <div class="card p-3">
+    <menus-create-page inline-template>
+
+        <div id="bigkahuna">
+
+            <div class="flex items-center flex-wrap mb-3">
+                <h1 class="w-full text-center mb-2 md:mb-0 md:text-left md:w-auto md:flex-1">Add menu</h1>
+            </div>
+
+            <div class="card p-1">
                 <div class="fieldset-builder">
                     <div class="form-group">
-                        <label class="block">Menu Name</label>
-                        <small class="help-block">The name will be converted to a slugified version that you'll use to reference in your templates (i.e. main-menu)</small>
+                        <label class="block">Menu name</label>
                         <input type="text" class="form-control mb-2" autofocus="autofocus" v-model="menuName">
+                        <small class="help-block">The name will be converted to a slugified version that you'll use to reference in your templates (i.e. main-menu)</small>
+                        <button type="button" class="btn btn-primary" v-on:click="saveMenu">Create menu</button>
                     </div>
                 </div>
             </div>
+
         </div>
-    </div>
-</menus-create-page>
+
+    </menus-create-page>
 
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,46 +2,52 @@
 
 @section('content')
 
-<menus-list-page inline-template>
-    <div class="flex items-center mb-3">
-        <h1 class="flex-1">Menus</h1>
-        <a href="{{ route('addons.menu_editor.create') }}" class="btn btn-primary">Add New Menu</a>
-    </div>
+    <menus-list-page inline-template>
 
-    <div class="card flush">
-        <div class="dossier-table-wrapper">
-            <table class="dossier">
-                <tbody>
-                    @foreach ($menus as $menu)
-                    <tr>
-                        <td class="cell-title first-cell">
-                            <span class="column-label">Title</span>
-                            <a href="{{ route('addons.menu_editor.edit', $menu) }}">
-                                {{ ucwords(str_replace('-', ' ', $menu)) }}
-                            </a>
-                        </td>
+        <div id="bigkahuna">
 
-                        <td class="cell-slug">
-                            <span class="column-label">Slug</span>
-                            {{ $menu }}
-                        </td>
+            <div class="flex items-center mb-3">
+                <h1 class="w-full text-center mb-2 md:mb-0 md:text-left md:w-auto md:flex-1">Menus</h1>
+                <a href="{{ route('addons.menu_editor.create') }}" class="btn btn-primary">Add New Menu</a>
+            </div>
 
-                        <td class="column-actions">
-                            <div class="btn-group action-more">
-                                <button type="button" class="btn-more dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="icon icon-dots-three-vertical"></i> </button>
-                                <ul class="dropdown-menu">
-                                    <li class="warning" @click="deleteMenu('{{ $menu }}')">
-                                        <a href="#">Delete</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <div class="card flush">
+                <div class="dossier-table-wrapper">
+                    <table class="dossier">
+                        <tbody>
+                            @foreach ($menus as $menu)
+                            <tr>
+                                <td class="cell-title first-cell">
+                                    <span class="column-label">Title</span>
+                                    <a href="{{ route('addons.menu_editor.edit', $menu) }}">
+                                        {{ ucwords(str_replace('-', ' ', $menu)) }}
+                                    </a>
+                                </td>
+
+                                <td class="cell-slug">
+                                    <span class="column-label">Slug</span>
+                                    {{ $menu }}
+                                </td>
+
+                                <td class="column-actions">
+                                    <div class="btn-group action-more">
+                                        <button type="button" class="btn-more dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="icon icon-dots-three-vertical"></i> </button>
+                                        <ul class="dropdown-menu">
+                                            <li class="warning" @click="deleteMenu('{{ $menu }}')">
+                                                <a href="#">Delete</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </div>
-    </div>
-</menus-list-page>
+
+    </menus-list-page>
 
 @endsection
