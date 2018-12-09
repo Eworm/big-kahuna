088b3f66928ebc833ff741f6da043612d01960f8
Wout <wout@woutmager.nl>
2018-12-09 16:28:20 +0100

    ..

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -16,7 +16,7 @@
                         <label class="block">Menu Name</label>
                         <input type="text" class="form-control mb-2" autofocus="autofocus" v-model="menuName">
                         <small class="help-block">The name will be converted to a slugified version that you'll use to reference in your templates (i.e. main-menu)</small>
-                        <button type="button" class="btn btn-default" v-on:click="saveMenu">
+                        <button type="button" class="btn btn-primary" v-on:click="saveMenu">
                             Save Menu
                         </button>
                     </div>
