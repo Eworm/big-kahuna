7fbe1c3f4eb21bb8fb4880705a9e010f78eaaf8c
Wout <wout@woutmager.nl>
2019-11-03 13:29:35 +0100

    Reverted the tag back to the pro localized version

--- BigKahuna/BigKahunaTags.php
+++ BigKahuna/BigKahunaTags.php
@@ -17,7 +17,7 @@ class BigKahunaTags extends Tags
         // Get the pages from storage and return proper html
         $pages = $this->storage->getJSON($this->getParam('menu'));
         if ($pages) {
-            return $this->getItems($pages);
+            return $this->getItems($pages['pages']);
         }
     }
 
@@ -26,96 +26,76 @@ class BigKahunaTags extends Tags
      *
      * @return html
      */
-    private function getItems($pages, $root = true)
-    {
-        $menu                   = ($this->getParam('menu')) ? ' ' . $this->getParam('menu') : "";
-        $id                     = ($this->getParam('id')) ? $this->getParam('id') : "";
-        $locales                = ($this->getParam('locales')) ? $this->getParam('locales') : "";
-        $class                  = ($this->getParam('class')) ? $this->getParam('class') : "nav";
-        $itemClass              = ($this->getParam('item_class')) ? $this->getParam('item_class') : "nav__item";
-        $parentClass            = ($this->getParam('parent_class')) ? $this->getParam('parent_class') : "nav__item--parent";
-        $submenu_class          = ($this->getParam('submenu_class')) ? $this->getParam('submenu_class') : "submenu";
-        $submenu_item_class     = ($this->getParam('submenu_item_class')) ? $this->getParam('submenu_item_class') : "submenu__item";
-        $activeClass            = ($this->getParam('active_class')) ? $this->getParam('active_class') : "is--active";
+     private function getItems($pages, $root = true)
+     {
+         $menu                   = ($this->getParam('menu')) ? ' ' . $this->getParam('menu') : "";
+         $id                     = ($this->getParam('id')) ? $this->getParam('id') : "";
+         $class                  = ($this->getParam('class')) ? $this->getParam('class') : "nav";
+         $itemClass              = ($this->getParam('item_class')) ? $this->getParam('item_class') : "nav__item";
+         $parentClass            = ($this->getParam('parent_class')) ? $this->getParam('parent_class') : "nav__item--parent";
+         $submenu_class          = ($this->getParam('submenu_class')) ? $this->getParam('submenu_class') : "submenu";
+         $submenu_item_class     = ($this->getParam('submenu_item_class')) ? $this->getParam('submenu_item_class') : "submenu__item";
+         $activeClass            = ($this->getParam('active_class')) ? $this->getParam('active_class') : "is--active";
 
-        $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+         $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-        $html = '';
+         $html = '';
 
-        if ($root == true) {
-            // The root list
-            $html .= '<ul id="' . $id . '" class="' . $class . $menu . '">';
-        } else {
-            // A submenu list
-            $itemClass = $submenu_item_class;
-            $html .= '<ul class="' . $submenu_class . '">';
-        }
+         if ($root == true) {
+             // The root list
+             $html .= '<ul id="' . $id . '" class="' . $class . $menu . '">';
+         } else {
+             // A submenu list
+             $itemClass = $submenu_item_class;
+             $html .= '<ul class="' . $submenu_class . '">';
+         }
 
-        foreach ($pages as $page) {
-            $id             = $page['id'];
-            $myClassname    = ' ' . $page['classname'];
-            $isParent       = $page['items'] ? ' ' . $parentClass : '';
-            $content        = Content::find($id);
-            $current_locale = site_locale();
+         foreach ($pages as $page) {
+             $id             = $page['id'];
+             $myClassname    = ' ' . $page['classname'];
+             $isParent       = $page['items'] ? ' ' . $parentClass : '';
 
-            if (isset($page['locales'])) {
+             if ($page['linktitle'] != '') {
+                 $myLinkTitle = $page['linktitle'];
+             } else {
+                 $myLinkTitle = $page['title'];
+             }
+             $content = Content::find($id);
 
-                if (count($page['locales']) > 1) {
-                    foreach ($page['locales'] as $locale) {
-                        if ($current_locale == $locale['locale']) {
-                            $localeTitle = $locale['title'];
-                            $localeUrl = $locale['url'];
-                        }
-                    }
-                } else {
-                    $localeTitle = $page['locales'][0]['title'];
-                    $localeUrl = $page['locales'][0]['url'];
-                }
-            } else {
-                $localeTitle = $page['title'];
-                $localeUrl = $page['url'];
-            }
+             if ($page['type'] == 'Custom') {
+                 // A custom link
+                 $html .= '<li class="' . $itemClass . $isParent . $myClassname .'">';
+             } else {
+                 $isactive = '';
+                 if ($content->absoluteUrl() == $actual_link || $this->getChildActiveStatus($page, $actual_link)) {
+                     $isactive = ' ' . $activeClass;
+                 }
 
-            if ($page['linktitle'] != '') {
-                $myLinkTitle = $page['linktitle'];
-            } else {
-                $myLinkTitle = $localeTitle;
-            }
+                 $html .= '<li class="' . $itemClass . $isParent . $isactive . $myClassname . '">';
+             }
 
-            if ($page['type'] == 'Custom') {
-                // A custom link
-                $html .= '<li class="' . $itemClass . $isParent . $myClassname .'">';
-            } else {
-                $isactive = '';
-                if ($content->absoluteUrl() == $actual_link || $this->getChildActiveStatus($page, $actual_link)) {
-                    $isactive = ' ' . $activeClass;
-                }
+             if ($page['type'] == 'Custom') {
+                 // A custom link
+                 $html .= '<a href="' . $page['url'] . '" title="' . $myLinkTitle . '" rel="external">';
+             } else {
+                 // An internal link
+                 $html .= '<a href="' . $content->absoluteUrl() . '" title="' . $myLinkTitle . '">';
+             }
 
-                $html .= '<li class="' . $itemClass . $isParent . $isactive . $myClassname . '">';
-            }
+             $html .= $page['title'];
+             $html .= '</a>';
 
-            if ($page['type'] == 'Custom') {
-                // A custom link
-                $html .= '<a href="' . $page['url'] . '" title="' . $myLinkTitle . '" rel="external">';
-            } else {
-                // An internal link
-                $html .= '<a href="' . $localeUrl . '" title="' . $myLinkTitle . '">';
-            }
+             if ($page['items']) {
+                 // Return the submenu html
+                 $html .= $this->getItems($page['items'], false);
+             }
 
-            $html .= $myLinkTitle;
-            $html .= '</a>';
+             $html .= '</li>';
+         }
 
-            if ($page['items']) {
-                // Return the submenu html
-                $html .= $this->getItems($page['items'], false);
-            }
-
-            $html .= '</li>';
-        }
-
-        $html .= '</ul>';
-        return $html;
-    }
+         $html .= '</ul>';
+         return $html;
+     }
 
     public function getChildActiveStatus($page, $actual_link)
     {
